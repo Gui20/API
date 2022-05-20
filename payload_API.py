261da9902eb3781d7fe1af0dd8a0168b33eaee66
Gui20 <coelho.gui19@gmail.com>
2022-05-20 16:08:54 -0300

    agr deu bom

--- payload_API.py
+++ payload_API.py
@@ -1,16 +1,19 @@
 from flask import Flask
+from sqlalchemy import create_engine
 import os
+
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
 
-@app.route('/api/v1/<string:x>', methods=['GET', 'POST'])
-def getUSD(x):
-    os.mkdir(x)
-    return x
+@app.route('/api/v1/<string:nome>&<string:idade>', methods=['GET', 'POST'])
+def getUSD(nome, idade):
+    engine = create_engine('postgresql://postgres:123456@serverioutility.hopto.org/teste')
+    engine.execute(f"INSERT INTO cachorro (nome,idade) VALUES ('{nome}','{idade}')")
+    return "deu bom"
 
 
-# port = int(os.environ.get('PORT', 5050))
-# app.run(host="0.0.0.0", port=port)
+port = int(os.environ.get('PORT', 5050))
+app.run(host="0.0.0.0", port=port)
 
