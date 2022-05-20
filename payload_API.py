bde191282b1d2a25572a45e41336b856c4d4746c
Gui20 <coelho.gui19@gmail.com>
2022-05-20 11:29:37 -0300

    teste

--- payload_API.py
+++ payload_API.py
@@ -7,11 +7,7 @@ app.config['DEBUG'] = True
 
 @app.route('/api/v1/<string:x>', methods=['GET', 'POST'])
 def getUSD(x):
-
     os.mkdir(x)
     return x
 
-
-# port = int(os.getenv('PORT'), 5000)
-print(int(os.getenv('PORT'), 5000))
-app.run(host="0.0.0.0", port=port)
+app.run(host="0.0.0.0")
