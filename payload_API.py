a49798ca43bae135612f3ae7b0fd3841965db2d4
Gui20 <coelho.gui19@gmail.com>
2022-05-20 16:24:51 -0300

    mais um teste

--- payload_API.py
+++ payload_API.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import requests
+from flask import Flask, request
 from sqlalchemy import create_engine
 import os
 
@@ -8,10 +9,12 @@ app.config['DEBUG'] = True
 
 
 @app.route('/api/v1/<string:nome>&<string:idade>', methods=['GET', 'POST'])
-def getUSD(nome, idade):
-    engine = create_engine('postgresql://postgres:123456@serverioutility.hopto.org/teste')
-    engine.execute(f"INSERT INTO cachorro (nome,idade) VALUES ('{nome}','{idade}')")
-    return "deu bom"
+def getPayload(payload):
+    if request.headers.get("Authorization") == "CachorroZika":
+        engine = create_engine('postgresql://postgres:123456@serverioutility.hopto.org/teste')
+        engine.execute(f"INSERT INTO cachorro (nome) VALUES ('{payload}'")
+        return "deu bom"
+    return "deu ruim"
 
 
 # port = int(os.environ.get('PORT', 5050))
