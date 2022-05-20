9e7d235e50b18de1a2bb7cf3db6c221c475ade9d
Gui20 <coelho.gui19@gmail.com>
2022-05-20 14:31:36 -0300

    make it better

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,8 @@
-from flask import Flask
+from API import app
 import os
-app = Flask(__name__)
-
-app.config['DEBUG'] = True
 
 
 @app.route('/api/v1/<string:x>', methods=['GET', 'POST'])
 def getUSD(x):
     os.mkdir(x)
     return x
-
-
-app.run(host="0.0.0.0")
-
